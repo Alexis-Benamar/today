c7cdf2143fe501aa6e00419cbfbb237a34e34765
Alexis-Benamar <alexis.benamar@viacesi.fr>
2024-02-13 17:04:44 +0100

    update netlify.toml & remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  server: "./server.ts",
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildPath: "build/index.js",
 };
