ab7dda19007b7138be1208021078e4c82a5363a1
Alexis-Benamar <alexis.benamar@viacesi.fr>
2024-05-28 16:32:21 +0200

    add data encryption on todo text

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import * as build from '@remix-run/dev/server-build'
 interface Env {
   SUPABASE_URL: string
   SUPABASE_PUBLIC_KEY: string
+  DATA_KEY: string
 }
 
 type Context = EventContext<Env, string, unknown>
@@ -18,6 +19,7 @@ const handleRequest = createPagesFunctionHandler({
   getLoadContext: (context: Context) => ({
     SUPABASE_URL: context.env.SUPABASE_URL,
     SUPABASE_PUBLIC_KEY: context.env.SUPABASE_PUBLIC_KEY,
+    DATA_KEY: context.env.DATA_KEY,
   }),
 })
 
